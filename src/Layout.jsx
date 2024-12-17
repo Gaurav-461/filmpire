96b215e69589ae1bb9f1e32cdffeb9996a511773
Gaurav-461 <mauryagaurav1612@gmail.com>
2024-12-18 02:18:46 +0530

    create layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,25 @@
-import { Outlet } from "react-router-dom"
-import { Footer, NavBar } from "./components/index.js"
+/* eslint-disable no-unused-vars */
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+
+import { Footer, NavBar } from "./components/index.js";
+import useStyles from "./styles";
 
 const Layout = () => {
+  const classes = useStyles();
+
   return (
     <>
+      <div className={classes.root}>
+        <CssBaseline />
         <NavBar />
-        <Outlet />
-        <Footer />
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Outlet />
+        </main>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
