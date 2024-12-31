0f5cd366ac92394f3a796cb8a5f912a4d7a38d9d
Gaurav-461 <mauryagaurav1612@gmail.com>
2024-12-31 19:00:46 +0530

    `Added conditional rendering for pagination component`

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -7,7 +7,7 @@ import Movie from '../Movie/Movie.jsx'
 const MoviesList = ({ movies, numberOfMovie }) => {
   const classes = useStyles();
   return (
-    <Grid2 container className={classes.moviesContainer}>
+    <Grid2 container className={classes.moviesContainer} columnGap={'5rem'}>
       {movies?.slice(0, numberOfMovie).map((movie, i) => (
         <Movie key={movie.id} movie={movie} i={i} />
       ))}
