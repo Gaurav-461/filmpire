09afe1132314cebaf39f0e908e4ad042b3430235
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-07-21 03:12:03 +0530

    Update MoviesList styles: ensure consistent spacing in moviesContainer

--- src/components/MoviesList/styles.js
+++ src/components/MoviesList/styles.js
@@ -4,6 +4,7 @@ export default makeStyles((theme) => ({
   moviesContainer: {
     display: "flex",
     flexWrap: "wrap",
+    justifyContent: "space-evenly",
     overflow: "hidden",
     [theme.breakpoints.down("sm")]: {
       justifyContent: "center",
